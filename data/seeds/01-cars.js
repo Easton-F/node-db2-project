e3ac5cb74683b29d1e54d444331d90828ad75b5d
Easton <easton.fritsch21@gmail.com>
2023-09-29 13:18:34 -0600

    wip

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,7 +1,7 @@
 // STRETCH
 const cars = [
     {
-        vin: '1111111111111',
+        vin: '1G8ZG5284TZ315929',
         make: 'toyota',
         model: 'prius',
         mileage: 215000,
@@ -9,14 +9,14 @@ const cars = [
         transmission: 'manual',
     },
     {
-        vin: '222222222222',
+        vin: 'WDDGF4HB1DR252311',
         make: 'toyota',
         model: 'corolla',
         mileage: 115000,
         title: 'salvage',
     },
     {
-        vin: '3333333333333',
+        vin: '1G6KS52Y4TU846196',
         make: 'ford',
         model: 'focus',
         mileage: 15000,
